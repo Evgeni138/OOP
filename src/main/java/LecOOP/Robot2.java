a838543161536785d517c878e02ec33adf5c2d74
Evgegeni <popovevgeni81@gmail.com>
2022-09-25 20:09:50 +0800

    OOP

---
+++ src/main/java/LecOOP/Robot2.java
@@ -0,0 +1,91 @@
+package LecOOP;
+
+public class Robot2 {
+    /**
+     * Уровень робота
+     */
+    private int level;
+    /**
+     * Имя робота
+     */
+    private String name;
+
+    /**
+     * @param name  Имя робота !Не должно начинаться с цифры
+     * @param level Уровень робота
+     */
+    public Robot2(String name, int level) {
+        this.name = name;
+        this.level = level;
+    }
+
+    // Методы вкл\выкл подсистем
+    public void powerOn() {
+        this.startBIOS();
+        this.startOS();
+        this.sayHi();
+    }
+
+    public void powerOff() {
+        this.sayBye();
+        this.stopOS();
+        this.stopBIOS();
+    }
+/** Создание гетеров для переменных name и level*/
+    public int getLevel() {
+        return this.level;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Загрузка BIOS
+     */
+    private void startBIOS() {
+        System.out.println("Start BIOS...");
+    }
+
+    /**
+     * Загрузка OS
+     */
+    private void startOS() {
+        System.out.println("Start OS...");
+    }
+
+    /**
+     * Приветствие
+     */
+    private void sayHi() {
+        System.out.println("Hello world");
+    }
+
+    /**
+     * Выгрузка BIOS
+     */
+    private void stopBIOS() {
+        System.out.println("Stop BIOS...");
+    }
+
+    /**
+     * Выгрузка OS
+     */
+    private void stopOS() {
+        System.out.println("Stop OS...");
+    }
+
+    /**
+     * Прощание
+     */
+    private void sayBye() {
+        System.out.println("Bye...");
+    }
+
+    /**
+     * Работа
+     */
+    public void work() {
+        System.out.println("Working...");
+    }
+}
