ff17ffbbe5140ee5d79b63c05587139e2588af24
Evgegeni <popovevgeni81@gmail.com>
2022-10-21 20:48:42 +0800

    Lesson_4_v5

--- src/main/java/calculator/Presenters/Presenter.java
+++ src/main/java/calculator/Presenters/Presenter.java
@@ -3,10 +3,16 @@ package calculator.Presenters;
 import calculator.Models.CalculationNumber;
 import calculator.Models.ComplexNumber;
 import calculator.Views.ComplexView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+
 // Описывается класс Presenter
+
 public class Presenter {
+    static Logger logger = LoggerFactory.getLogger(Presenter.class);
+
     // Задаются поля для создания конструктора класса Presenter с экземплярами обладающими
     // методами CalculationNumber и ComplexView
     CalculationNumber calculationNumber;
@@ -24,15 +30,19 @@ public class Presenter {
         System.out.println(key1);
         if (key1.equals("1")) {
             sb.append("ComplexNumbers: ");
+            logger.info("ComplexNumbers: ");
         }
-//        else {
-//            sb.append("RationalNumbers: ");
-//        }
+        else {
+            sb.append("RationalNumbers: ");
+            logger.info("RationalNumbers: ");
+        }
         ArrayList<Double> x = complexView.setNumber(key1);
         ArrayList<Double> y = complexView.setNumber(key1);
 
         sb.append(x);
+        logger.info(String.valueOf(x));
         sb.append(y);
+        logger.info(String.valueOf(y));
         String result;
         String key = complexView.getAction();
         switch (key) {
@@ -40,21 +50,29 @@ public class Presenter {
                 result = calculationNumber.sum(new ComplexNumber(x.get(0), x.get(1)), new ComplexNumber(y.get(0), y.get(1)));
                 sb.append(key);
                 sb.append(result);
+                logger.info(key);
+                logger.info(result);
                 return result;
             case "-":
                 result = calculationNumber.sub(new ComplexNumber(x.get(0), x.get(1)), new ComplexNumber(y.get(0), y.get(1)));
                 sb.append(key);
                 sb.append(result);
+                logger.info(key);
+                logger.info(result);
                 return result;
             case "*":
                 result = calculationNumber.mult(new ComplexNumber(x.get(0), x.get(1)), new ComplexNumber(y.get(0), y.get(1)));
                 sb.append(key);
                 sb.append(result);
+                logger.info(key);
+                logger.info(result);
                 return result;
             case "/":
                 result = calculationNumber.div(new ComplexNumber(x.get(0), x.get(1)), new ComplexNumber(y.get(0), y.get(1)));
                 sb.append(key);
                 sb.append(result);
+                logger.info(key);
+                logger.info(result);
                 return result;
             default:
                 return null;
--- src/main/java/calculator/Program.java
+++ src/main/java/calculator/Program.java
@@ -3,18 +3,23 @@ package calculator;
 import calculator.Models.CalculationNumber;
 import calculator.Presenters.Presenter;
 import calculator.Views.ComplexView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
+    static Logger logger = LoggerFactory.getLogger(Program.class);
     public static void main(String[] args) {
 
         Presenter presener = new Presenter(new CalculationNumber(), new ComplexView());
         System.out.println(presener.calculatorPresenter());
         System.out.println(presener.sb);
 
-        try (FileWriter writer = new FileWriter("src/main/java/calculator/dataCalc.db", true)) {
+        logger.info(String.valueOf(presener.sb));
+
+        try (FileWriter writer = new FileWriter("src/main/java/calculator/dataCalc.txt", true)) {
             writer.append("\n" + presener.sb);
             writer.flush();
         } catch (IOException ex) {
